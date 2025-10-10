b2484193bf2f6c4fe76b068a63a21ffd606dfe49
Luis Leyva <luisleyva.bq@gmail.com>
2025-10-10 08:21:37 +0200

    truly n-planet now

--- code/planets_remix.m
+++ code/planets_remix.m
@@ -9,30 +9,59 @@ T = 0:e:tf;
 % CONSTANTS
 G = 3;
 M = 30;
-m = 1.1e-1;
-
-% DEFINE V & F
-V = @(q) - G*m*M * (q(1)^2 + q(2)^2)^(-1/2);
-F = @(q) [(- G*m*M * q(1) * (q(1)^2 + q(2)^2)^(-3/2))...
-          (- G*m*M * q(2) * (q(1)^2 + q(2)^2)^(-3/2))];
+all_m = [1.1e-1 1.3e-1];
+nplanets = 2;
 
 % DEFINE P & Q
-P(1,:) = [0.3 -0.8];
-Q(1,:) = [1 1];
+all_P(1,1,:) = [0.3 -0.8];
+all_P(1,2,:) = [-0.1 0.9];
+all_Q(1,1,:) = [1 1];
+all_Q(1,2,:) = [-1 -1];
 
-% DEFINE HAMILTONIAN
-H(1,:) = dot(P(1,:),P(1,:))/(2*m) + V(Q(1,:));
+for n=1:nplanets
+    P = squeeze(all_P(:,n,:))';
+    Q = squeeze(all_Q(:,n,:))';
+    m = all_m(n);
+    
+    % DEFINE V & F
+    V = @(q) - G*m*M * (q(1)^2 + q(2)^2)^(-1/2);
+    F = @(q) [(- G*m*M * q(1) * (q(1)^2 + q(2)^2)^(-3/2))...
+              (- G*m*M * q(2) * (q(1)^2 + q(2)^2)^(-3/2))];
+    
+    % DEFINE HAMILTONIAN
+    H(1,:) = dot(P(1,:),P(1,:))/(2*m) + V(Q(1,:));
+    
+    % INTEGRATION METHODS
+    [all_EC_P(:,n,:),...
+     all_EC_Q(:,n,:),...
+     all_EC_H(:,n,:)] = euler_cromer(P,Q,F,V,H,m,e,nstep);
+    
+    [all_LF_P(:,n,:),...
+     all_LF_Q(:,n,:),...
+     all_LF_H(:,n,:)] = leapfrog(P,Q,F,V,H,m,e,nstep);
+    
+    [all_RK2_P(:,n,:),...
+     all_RK2_Q(:,n,:),...
+     all_RK2_H(:,n,:)] = RK2(P,Q,F,V,H,m,e,nstep);
+end
 
-% INTEGRATION METHODS
-[EC_P,EC_Q,EC_H] = euler_cromer(P,Q,F,V,H,m,e,nstep);
-[LF_P,LF_Q,LF_H] = leapfrog(P,Q,F,V,H,m,e,nstep);
-[RK2_P,RK2_Q,RK2_H] = RK2(P,Q,F,V,H,m,e,nstep);
+% SUM HAMILTONIANS WIP
+% all_EC_H(:,n,) =
+% all_LF_H
+% all_RK2_H
 
 %% PLOTS
-plot3(RK2_P(:,1), RK2_P(:,2), T, 'x-',...
-      LF_P(:,1) , LF_P(:,2) , T, 'o-',...
-      EC_P(:,1) , EC_P(:,2) , T, 's-');
-legend('RK2', 'LF', 'EC');
+hold on
+for n=1:nplanets
+    plot3(all_RK2_P(:,n,1), all_RK2_P(:,n,2), T, 'x-',...
+          all_LF_P(:,n,1) , all_LF_P(:,n,2) , T, 'o-',...
+          all_EC_P(:,n,1) , all_EC_P(:,n,2) , T, 's-');
+    legend(['P' num2str(n) ' - RK2'], ...
+           ['P' num2str(n) ' - LF'], ...
+           ['P' num2str(n) ' - EC']);
+    pause;
+end
+hold off
 title('Momenta over time');
 pause;
 
@@ -42,6 +71,7 @@ plot3(RK2_Q(:,1), RK2_Q(:,2), T, 'x-',...
 legend('RK2', 'LF', 'EC');
 title('Position over time');
 pause;
+
 
 plot(T, RK2_H(:,1), 'x-',...
      T, LF_H(:,1) , 'o-',...
