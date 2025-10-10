cb07b25900088147495a06a0d898360fe71e0051
Luis Leyva <luisleyva.bq@gmail.com>
2025-10-10 05:51:50 +0200

    leapfrog. more like MIDfrog

--- code/functions/leapfrog.m
+++ code/functions/leapfrog.m
@@ -1,16 +1,36 @@
-function [outputArg1,outputArg2] = leapfrog(inputArg1,inputArg2)
-%LEAPFROG Summary of this function goes here
-%   Detailed explanation goes here
+function [P,Q,H] = leapfrog(P,Q,F,V,H,M,e,nstep)
+%LEAPFROG Leapfrog integration
+%   A function for LF integration.
 arguments (Input)
-    inputArg1
-    inputArg2
+    P % MOMENTA matrix
+    Q % POSITIONS / Degrees of freedom matrix
+    F % FORCE function
+    V % POTENTIAL force function
+    H % HAMILTONIAN matrix
+    M % MASS matrix
+    e % EPSILON time step
+    nstep % Number of steps
 end
 
 arguments (Output)
-    outputArg1
-    outputArg2
+    P % Full MOMENTA matrix with nstep entries
+    Q % Full POSITIONS / DoF matrix with nstep entries
+    H % Full HAMILTONIAN matrix with nstep entries
 end
 
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
+% First half step
+P(2) = P(1) + e/2 * F(Q(1));
+
+% Middle steps
+for t=2:nstep
+    Q(t) = Q(t-1) + e * P(t)/M;
+    P(t+1) = P(t) + e * F(Q(t));
+    H(t) = 1/2 * P(t+1)^2 / M + V(Q(t));
+end
+
+% Last half step
+Q(nstep+1) = Q(nstep) + e * P(nstep+1)/M;
+%P(nstep+2) = P(nstep+1) + e/2 * F(Q(nstep+1));
+H(nstep+1) = 1/2 * P(nstep+1)^2 / M + V(Q(nstep+1));
+
 end
