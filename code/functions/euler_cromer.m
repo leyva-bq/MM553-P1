7e170a8c960d6229013c0887e21d16bcafed8ebb
Luis Leyva <luisleyva.bq@gmail.com>
2025-10-10 05:51:31 +0200

    i dont need time actually

--- code/functions/euler_cromer.m
+++ code/functions/euler_cromer.m
@@ -1,4 +1,4 @@
-function [P,Q,H,T] = euler_cromer(P,Q,F,V,H,M,T,e,nstep)
+function [P,Q,H] = euler_cromer(P,Q,F,V,H,M,e,nstep)
 %EULER_CROMER Euler-Cromer integration
 %   A function for EC integration.
 arguments (Input)
@@ -8,7 +8,6 @@ arguments (Input)
     V % POTENTIAL force function
     H % HAMILTONIAN matrix
     M % MASS matrix
-    T % TIME matrix
     e % EPSILON time step
     nstep % Number of steps
 end
@@ -17,7 +16,6 @@ arguments (Output)
     P % Full MOMENTA matrix with nstep entries
     Q % Full POSITIONS / DoF matrix with nstep entries
     H % Full HAMILTONIAN matrix with nstep entries
-    T % Full TIME matrix with nstep entries
 end
 
 for t=1:nstep
@@ -25,7 +23,6 @@ for t=1:nstep
     Q(t+1) = Q(t) + e * P(t+1)/M; % Change P(i+1) to P(i)
                                      % for EC to Euler, respectively
     H(t+1) = 1/2 * P(t+1)^2 / M + V(Q(t+1));
-    T(t+1) = T(t) + e;
 end
 
 end
